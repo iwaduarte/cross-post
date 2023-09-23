b853f77a4081c519ddba1f1997f0c23ddb790e4d
iwaduarte <iwaduarte@gmail.com>
2023-09-23 01:18:10 -0300

    feat: dev.to info for formatter

--- src/platforms/devto.js
+++ src/platforms/devto.js
@@ -1,7 +1,26 @@
 const axios = require('axios');
 const Configuration = require('conf');
-
 const configstore = new Configuration();
+
+/** Format markdown to cater for specific dev.to rules:
+ * Frontmatter
+ * - properties should be unquoted i.e. title: "Unquoted" (wrong) => Unquoted (correct)
+ * - date property should be at least 15 min older (if older post, property should be removed)
+ * - tags property should have a maximum of 4 tags
+ * - publish property should be set in case public post is wanted
+ * - canonical_url has higher precedence that the variable at the POST request
+ *
+ * UI Recommendations:
+ * - It complains if titles are h1<#> or h3 <###>. It recommends h2<##>
+ *
+ * Embeds (Body transformation):
+ * embed files should add the {% <name of embed> <link> %} i.e.
+ * https://youtube.com/aWASKDAS => {% youtube https://youtube.com/aWASKDAS %}
+ * https://codepen.com/whatever => {% codepen https://codepen.com/aWASKDAS %}
+ * *
+ *
+ */
+const formatMarkdownForDev = () => {};
 
 /**
  * Post article to dev.to
@@ -11,9 +30,8 @@ const configstore = new Configuration();
  * @param {string} canonicalUrl URL of original article
  * @param {string} mainImage Cover image URL
  * @param {boolean} published whether to publish as draft or public
- * @param {null|Function} cb callback function to run after posting is finished
  */
-function postToDev(title, bodyMarkdown, canonicalUrl, mainImage, published, cb = null) {
+function postToDev(title, bodyMarkdown, canonicalUrl, mainImage, published) {
   const article = {
     title,
     published,
