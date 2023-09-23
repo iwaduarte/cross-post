f3162df54684a4e0570feaeaeb745e4de0c4e293
iwaduarte <iwaduarte@gmail.com>
2023-09-23 16:31:51 -0300

    feat: add tags support - create function to query tags - cached tags - remove unnecessary .then

--- src/platforms/hashnode.js
+++ src/platforms/hashnode.js
@@ -1,59 +1,93 @@
 const axios = require('axios');
-const Conf = require('conf');
+const Configuration = require('conf');
+const { hashnodeTags } = require('./hashnodeTags');
+const store = new Configuration();
+const { publicationId, apiKey } = store.get('hashnode');
 
-const configstore = new Conf();
+/** Format markdown to cater for specific hashnode rules:
+ * Frontmatter
+ * Hashnode does not support markdown. It has to be removed from the file
+ *
+ *
+ * UI Recommendations:
+ * -
+ *
+ * Embeds (Body transformation):
+ *
+ *
+ */
+const formatMarkdownForHashnode = () => {};
+
+const getTagsId = () => {
+  return axios
+    .post(
+      'https://api.hashnode.com',
+      {
+        query: `query Tags {  tagCategories(slug:"javascript"){ _id, name, slug }}`
+      },
+      {
+        headers: {
+          Authorization: apiKey
+        }
+      }
+    )
+    .then(({ data }) => {
+      data.data.tagCategories.forEach(obj => console.log(obj));
+    });
+};
 
 /**
  * Post article to Hashnode
  *
  * @param {string} title Title of article
- * @param {string} contentMarkdown Content of article in Markdown
- * @param {string} originalArticleURL URL of original article
- * @param {string} coverImageURL URL of cover image
- * @param {boolean} hideFromHashnodeFeed Whether to post it publically or not
+ * @param {string} markdown Content of article in Markdown
+ * @param {string} canonicalURL URL of original article
+ * @param {string} mainImage URL of cover image
+ * @param {boolean} shouldPublish Whether to post it publicly or not
+ *
+ *
  */
-function postToHashnode(title, contentMarkdown, originalArticleURL, coverImageURL, hideFromHashnodeFeed) {
-  const configData = configstore.get('hashnode');
+function postToHashnode({ title, markdownBody, canonicalURL, mainImage, tags = [], shouldPublish }) {
   const data = {
     input: {
-      title,
-      contentMarkdown,
-      isPartOfPublication: { publicationId: configData.publicationId },
-      tags: []
+      // title,
+      contentMarkdown: markdownBody,
+      isPartOfPublication: { publicationId },
+      //using existing tags
+      tags: tags
+        .map(
+          tag =>
+            hashnodeTags[tag] ||
+            hashnodeTags[tag.replace(/[^a-zA-Z0-9-]/g, '')] ||
+            hashnodeTags[tag.replace(/[^a-zA-Z0-9]/g, '')] ||
+            hashnodeTags[tag.split('-')[0]] ||
+            hashnodeTags[tag.split('-')[1]] ||
+            null
+        )
+        .filter(Boolean),
+      ...(canonicalURL ? { isRepublished: { originalArticleURL: canonicalURL } } : {}),
+      ...(mainImage ? { coverImageURL: mainImage } : {})
     },
-    publicationId: configData.publicationId,
-    hideFromHashnodeFeed
+    publicationId: publicationId,
+    hideFromHashnodeFeed: !shouldPublish
   };
-  if (originalArticleURL) {
-    data.isRepublished = {
-      originalArticleURL
-    };
-  }
-  if (coverImageURL) {
-    data.input.coverImageURL = coverImageURL;
-  }
+
   return axios
     .post(
       'https://api.hashnode.com',
       {
-        query:
-          'mutation createPublicationStory($input: CreateStoryInput!, $publicationId: String!){ createPublicationStory(input: $input, publicationId: $publicationId){ post { slug, publication { domain } } } }',
+        query: `mutation createPublicationStory($input: CreateStoryInput!, $publicationId: String!, $hideFromHashnodeFeed: Boolean ){ \
+         createPublicationStory(input: $input, publicationId: $publicationId, hideFromHashnodeFeed: $hideFromHashnodeFeed ){ \
+          message } }`,
         variables: data
       },
       {
         headers: {
-          Authorization: configData.apiKey
+          Authorization: apiKey
         }
       }
     )
-    .then(res => {
-      if (res.data.errors) {
-        throw new Error(`Error occured while cross posting to Hashnode: ${res.data.errors[0].message}`);
-      } else {
-        const { post } = res.data.data.createPublicationStory;
-        const postUrl = `${post.publication.domain ? post.publication.domain : ''}/${post.slug}`;
-      }
-    });
+    .then(({ data }) => data);
 }
 
 module.exports = postToHashnode;
