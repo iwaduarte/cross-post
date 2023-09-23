18f27c9ad3d7967a17a89dd3ca8912cd254330e1
iwaduarte <iwaduarte@gmail.com>
2023-09-23 16:30:25 -0300

    fix: canonicalUrl spelling to URL - add spread  instead of if()

--- src/platforms/devto.js
+++ src/platforms/devto.js
@@ -27,21 +27,19 @@ const formatMarkdownForDev = () => {};
  *
  * @param {string} title Title of article
  * @param {string} markdown Content of the article in markdown
- * @param {string} canonicalUrl URL of original article
+ * @param {string} canonicalURL URL of original article
  * @param {string} mainImage Cover image URL
  * @param {boolean} published whether to publish as draft or public
  */
-function postToDev({ title, markdown, canonicalUrl, mainImage, published }) {
+function postToDev({ title, markdown, canonicalURL, mainImage, published }) {
   const article = {
     title,
     published,
     body_markdown: markdown,
-    canonical_url: canonicalUrl
+    canonical_url: canonicalURL,
+    ...(mainImage ? { main_image: mainImage } : {})
   };
-  if (mainImage) {
-    article.main_image = mainImage;
-  }
-  // send article to DEV.to
+
   return axios.post(
     'https://dev.to/api/articles',
     {
