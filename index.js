cc06f53b69c6e61d53f8099fd6447ca875c04334
iwaduarte <iwaduarte@gmail.com>
2023-09-25 10:24:45 -0300

    feat:  less commands and more succinct ux

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 #! /usr/bin/env node
-const path = require('path');
 const { program } = require('commander');
 const run = require('./src/run');
+const { reset } = require('./src/config/reset');
+const { setkey } = require('./src/config/setk');
 
 const ALLOWED_PLATFORMS = ['dev.to', 'hashnode', 'medium'];
+
 program.usage('[command] [options]');
 
+program.command('clear').description(`Resets all keys.`).action(reset);
 program
-  .command('run <url>')
-  .description('Cross post a blog post')
+
+  .command('setk')
+  .argument('<platform>', 'Configure key for hashnode.com | medium.com | dev.to  platforms')
+  .description(`Add configuration for a platfor. Allowed values are: ${ALLOWED_PLATFORMS}`)
+  .action(setkey);
+
+program
+  .argument('<url>')
+  .description('Crossposting articles everywhere')
   .option(
-    '-cnu, --canonical-url [canonicalUrl]',
+    '-c, --canonical-url [canonicalUrl]',
     'Original url the article was published first. It is mandatory for SEO'
   )
-  .option('-t, --title [title]', 'Title for the article')
-  .option('-iu, --image-url [imageUrl]', "URL of image to use for the article's main image.")
+  .option('-t, --title <title>', 'Title for the article')
+  .option('-tg, --tags <tags...>', 'Tags for the article')
+  .option('-i, --image-url <imageUrl>', "URL of image to use for the article's main image.")
+  .option('-si, --skip-image', 'Skip image main cover.')
   .option(
     '-p, --platforms [platforms...]',
-    `Platforms to post articles to. Allowed values are: ${ALLOWED_PLATFORMS}
-    )}`
+    `Platforms to post articles to. Allowed values are: ${ALLOWED_PLATFORMS}`
   )
   .option('-pu, --publish', 'Publish to the platform. Default: draft.')
-
   .action(run);
-
-program
-  .command('config')
-  .description(`Add configuration for a platform or other options. Allowed values are: ${ALLOWED_PLATFORMS}`)
-  .executableDir(path.join(__dirname, './src/config'))
-  .command('dev', 'configure for dev.to platform')
-  .command('medium', 'configure for medium.com platform')
-  .command('hashnode', 'configure for hashnode.com platform')
-  .command('reset', 'reset configuration for a given platform(s)');
 
 program.parse();
